f967bfc4a1733bd41c0c594eeb85f4c2c842897a
Vladimir Kutepov <frenzzy.man@gmail.com>
2013-04-30 23:33:52 +0400

    homework1 code optimize

--- homework1/script.js
+++ homework1/script.js
@@ -1,29 +1,38 @@
-/*
+/**
  * Функция multiplyArrays
  * - может принимать на вход произвольное количество массивов
  * - каждый из этих массивов может содержать произвольное количество чисел
  * - функция возвращает массив, количество элементов этого массива равно количеству переданных массивов
  * - каждый элемент возвращаемого массива - результат перемножения элементов соответствующего принятого массива
+ *
+ * @returns {Array}
  */
 function multiplyArrays() {
+    "use strict";
+    var resultArray = [],
+        argumentsLength = arguments.length,
+        argumentArray,
+        argumentArrayLength,
+        product,
+        i,
+        j;
 
-    var array = [];
+    for (i = 0; i < argumentsLength; i++) {
+        argumentArray = arguments[i];
+        argumentArrayLength = argumentArray.length;
+        product = argumentArray[0];
 
-    for (var i = 0; i < arguments.length; i++) {
-
-        var product = arguments[i][0];
-
-        for (var j = 1; j < arguments[i].length; j++) {
-            product *= arguments[i][j];
+        for (j = 1; j < argumentArrayLength; j++) {
+            product *= argumentArray[j];
         }
 
-        array.push(product);
+        resultArray.push(product);
     }
 
-    return array;
+    return resultArray;
 }
 
 // Проверка
 console.log(
-    multiplyArrays([1,2], [12,5,7], [1])
+    multiplyArrays([1, 2], [12, 5, 7], [1])
 );
