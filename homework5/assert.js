55aa52bbca11897ec1e24afdaf248c2203f2c59c
Vladimir Kutepov <frenzzy.man@gmail.com>
2013-05-26 19:48:52 +0400

    homework5 comments

--- homework5/assert.js
+++ homework5/assert.js
@@ -1,20 +1,46 @@
+/**
+ * Расширят console.asset для работы с console.log
+ * @type {Object}
+ */
 var assert = {};
 
+/**
+ * Хранит сообщения выводимые console.log
+ * @type {Array}
+ */
 assert.result = [];
+
+/**
+ * Хранит встроенный метод console.log
+ * @type {Function}
+ */
 assert.consoleLog = console.log;
 
-assert.log = function (str) {
+/**
+ * Эмулирует console.log сохраняя аргументы в массив
+ * @returns {*} Возвращает тоже что console.log
+ */
+assert.log = function () {
     "use strict";
-    assert.result.push(str);
+    Array.prototype.forEach.call(arguments, function (arg) {
+        assert.result.push(arg);
+    });
     return assert.consoleLog.apply(console, arguments);
 };
 
+/**
+ * Сравнивает выводимые логи с ожидаемыми и выводит сообщение если не совпадают
+ * @param {Function} callback Функция которая содержит любой код, использующий console.log
+ * @param {Array} expected Ожидаемые результаты
+ * @param {String} message Сообщение при не верном результате
+ */
 assert.shouldLog = function (callback, expected, message) {
     "use strict";
-    console.log = this.log;
+    console.log = assert.log;
     callback();
-    expected.forEach(function (element, index) {
-        console.assert(assert.result[index] === element, message);
+    expected.forEach(function (str, i) {
+        console.assert(assert.result[i] === str, message);
     });
-    console.log = this.consoleLog;
+    console.log = assert.consoleLog;
+    assert.result = [];
 };
