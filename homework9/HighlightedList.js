2a55405ada72d2063f56ad8a1294d09674e36703
Vladimir Kutepov <frenzzy.man@gmail.com>
2013-06-22 13:52:10 +0400

    hw9 Update

--- homework9/HighlightedList.js
+++ homework9/HighlightedList.js
@@ -18,7 +18,7 @@ inherit(List, HighlightedList);
  * Добавляет класс .highlight для списка
  * @private
  */
-HighlightedList.prototype._highlightSelf = function () {
+HighlightedList.prototype._highlightSelfToggle = function () {
     "use strict";
 
     var className = this._element.className,
@@ -38,9 +38,9 @@ HighlightedList.prototype._highlightSelf = function () {
 HighlightedList.prototype.addItem = function (item) {
     "use strict";
 
-    this._highlightSelf();
-    setTimeout(this._highlightSelf.bind(this), 100);
-    setTimeout(this._highlightSelf.bind(this), 200);
-    setTimeout(this._highlightSelf.bind(this), 300);
+    this._highlightSelfToggle();
+    setTimeout(this._highlightSelfToggle.bind(this), 100);
+    setTimeout(this._highlightSelfToggle.bind(this), 200);
+    setTimeout(this._highlightSelfToggle.bind(this), 300);
     return this.constructor.addItem(item); // TODO: проверить правильность записи
 };
